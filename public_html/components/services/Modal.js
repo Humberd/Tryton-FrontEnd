5681cf6e885a2197c8d7b8fa1d1880e1e7075f46
Humberd <sawik11@wp.pl>
2016-06-11 14:11:14 +0200

    finished modal service

--- public_html/components/services/Modal.js
+++ public_html/components/services/Modal.js
@@ -1,13 +1,16 @@
 var app = angular.module("TrytonApp");
 app.service("Modal", function ($uibModal, ViewUrl, Logger) {
-    return function ModalFunction(htmlFile, controller, resolvers) {
+    return function ModalFunction(htmlFile, controller, modalSizeClass, resolvers) {
         if (!angular.isString(htmlFile)) {
             Logger.warning("[%o] must be a 'htmlFile' string value, but instead is [%s]", htmlFile, typeof (htmlFile));
             return;
         } else if (!angular.isString(controller)) {
             Logger.warning("[%o] must be a 'controller' string value, but instead is [%s]", controller, typeof (controller));
             return;
-        } else if (angular.isDefined(resolvers) && !angular.isObject()) {
+        } else if (angular.isDefined(modalSizeClass) && !angular.isString(modalSizeClass)) {
+            Logger.warning("[%o] must be a 'modalSizeClass' string value, but instead is [%s]", controller, typeof (modalSizeClass));
+            return;
+        } else if (angular.isDefined(resolvers) && !angular.isObject(resolvers)) {
             Logger.warning("[%o] must be a 'resolvers' object type, but instead is [%s]", resolvers, typeof (resolvers));
             return;
         }
@@ -18,7 +21,7 @@ app.service("Modal", function ($uibModal, ViewUrl, Logger) {
             keyboard: true, //czy wylaczyc modal klawiszem ESC
             resolve: resolvers, //obiekt zmiennych przekazywanych do controllera,
             templateUrl: ViewUrl + "modal/" + htmlFile,
-            windowTopClass: "bvcbcvbdfbdbd"
+            size: modalSizeClass
         });
     };
 });
