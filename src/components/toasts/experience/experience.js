fc77c135708219a8002c30ec4398e62fef5bd3b0
Humberd <sawik11@wp.pl>
2016-07-14 01:23:59 +0200

    added showing exp toast on click with timeout.

--- src/components/toasts/experience/experience.js
+++ src/components/toasts/experience/experience.js
@@ -2,15 +2,43 @@ angular.module("TrytonApp.Toasts", [])
     .factory("Exp", ExpToastFactory)
     .directive("expToast", ExpToastDirective);
 
-function ExpToastFactory($document, $sce, $rootScope, $compile) {
-    let body = $document[0].body;
+function ExpToastFactory($document, $sce, $rootScope, $compile, $timeout) {
+    let _body = $document[0].body;
+    let _toast = angular.element("<exp-toast>");
+    let _isDisplayed = false;
+    let _configs = {
+        hideDelay: 4000
+    };
+    let _timeoutPromise;
     return {
-        foo: function() {
-            // let toast = angular.element("<exp-toast>");
-            // angular.element(body).append($compile("<exp-toast></exp-toast>")(null));
-            let el = $compile("<exp-toast></exp-toast>")($rootScope.$new());
-            angular.element(body).append(el);
+        show: function() {
+            if (!_isDisplayed) {
+                _isDisplayed = true;
+
+                let compiledToast = $compile(_toast)($rootScope.$new());
+                angular.element(_body).append(compiledToast);
+
+                this._setTimeout();
+            } else {
+                this._cancelTimeout();
+                this._setTimeout();
+            }
         },
+        hide: function() {
+            _toast.remove();
+            _isDisplayed = false;
+        },
+        _setTimeout: function() {
+            var self = this;
+            _timeoutPromise = $timeout(function() {
+                self.hide();
+            }, _configs.hideDelay);
+        },
+        _cancelTimeout: function() {
+            $timeout.cancel(_timeoutPromise);
+        },
+
+
     };
 }
 
@@ -18,6 +46,7 @@ function ExpToastDirective(ViewUrl) {
     return {
         restrict: "E",
         templateUrl: ViewUrl + "experience.html",
+        replace: true,
         link: function(scope, elem, attrs) {
 
         }
--- src/indexController.js
+++ src/indexController.js
@@ -3,11 +3,13 @@ angular.module("TrytonApp")
 
 function indexController($scope, Exp, $mdToast) {
     $scope.foo = "";
-    Exp.foo();
+    // $scope.click = function() {
+    //     $mdToast.showSimple({
+    //         textContent: "Hello",
+    //         hideDelay: 100000
+    //     });
+    // }
     $scope.click = function() {
-        $mdToast.showSimple({
-            textContent: "Hello",
-            hideDelay: 100000
-        });
+    	Exp.show();
     }
 }
