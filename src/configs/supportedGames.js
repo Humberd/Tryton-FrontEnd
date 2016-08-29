ba3fa22b57970af1786191e5c6f3e1e97a8c24ae
Humberd <sawik11@wp.pl>
2016-08-29 23:25:58 +0200

    added cs go to supported games; added iconUrls

--- src/configs/supportedGames.js
+++ src/configs/supportedGames.js
@@ -5,6 +5,7 @@
 		.config(SupportedGamesConfig);
 
 	function SupportedGamesConfig(SupportedProvider) {
-		SupportedProvider.games.add("League of Legends", "LoL", true);
+		SupportedProvider.games.add("League of Legends", "LoL", true, "lol.png");
+		SupportedProvider.games.add("Counter Strike: Global Offensive", "CS:GO",  true, "csgo.png");
 	}
 })();
