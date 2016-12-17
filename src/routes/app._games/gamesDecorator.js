bd68c6bd2e6d0806036eb519635c1167b2539db5
Humberd <sawik11@wp.pl>
2016-12-17 22:30:16 +0100

    Changed ._games states controller names formula, because controllers with same names would overlap eachother Then "LolController". Now depending on a state, for example "dashboard_lol.controller".

--- src/routes/app._games/gamesDecorator.js
+++ src/routes/app._games/gamesDecorator.js
@@ -1,52 +1,51 @@
-(function() {
-    "use strict";
+(function () {
+	"use strict";
 
-    angular.module("TrytonApp.Router.App._games")
-        .config(GamesDecoratorConfig);
+	angular.module("TrytonApp.Router.App._games")
+		.config(GamesDecoratorConfig);
 
-    function GamesDecoratorConfig($stateProvider, ViewUrl) {
+	function GamesDecoratorConfig($stateProvider, ViewUrl) {
 
-        /*
-        State decorator działa w ten sposób, że jako parametr pierwszy
-        podaję nazwę pola, które chcę nadpisać, które ustaliłem wcześniej
-        przy deklarowaniu $stateProvider.state();
-        Funkcję w parametrze drugim wywoła tyle razy, ile statów zadeklarowałem.
-        Zwrócić trzeba wartość, która nadpisze element ustalony w parametrze pierwszym
-        */
-        $stateProvider.decorator("templateUrl", function(state, parent) {
-            //ustalać będę tylko dla tych elementów, które dziedziczą z app._games
-            if (includes(state, "app._games") && state.name !== "app._games") {
-                //wydobywam nazwę tego statu, czyli, np. dashboard zamiast app._games.dashboard
-                var dottedNames = state.self.name.split(".");
-                var lastStateName = dottedNames[dottedNames.length - 1];
+		/*
+		 State decorator działa w ten sposób, że jako parametr pierwszy
+		 podaję nazwę pola, które chcę nadpisać, które ustaliłem wcześniej
+		 przy deklarowaniu $stateProvider.state();
+		 Funkcję w parametrze drugim wywoła tyle razy, ile statów zadeklarowałem.
+		 Zwrócić trzeba wartość, która nadpisze element ustalony w parametrze pierwszym
+		 */
+		$stateProvider.decorator("templateUrl", function (state, parent) {
+			//ustalać będę tylko dla tych elementów, które dziedziczą z app._games
+			if (includes(state, "app._games") && state.name !== "app._games") {
+				//wydobywam nazwę tego statu, czyli, np. dashboard zamiast app._games.dashboard
+				var dottedNames = state.self.name.split(".");
+				var lastStateName = dottedNames[dottedNames.length - 1];
 
-                var templateUrl = function($stateParams) {
-                    return ViewUrl + lastStateName + "_" + $stateParams.game + ".html";
-                };
-                return templateUrl;
-            } else {
-                return state.self.templateUrl;
-            }
-        });
-        $stateProvider.decorator("controllerProvider", function(state, parent) {
-            if (includes(state, "app._games") && state.name !== "app._games") {
-                var dottedNames = state.self.name.split(".");
-                var lastStateName = dottedNames[dottedNames.length - 1];
+				var templateUrl = function ($stateParams) {
+					return ViewUrl + lastStateName + "_" + $stateParams.game + ".html";
+				};
+				return templateUrl;
+			} else {
+				return state.self.templateUrl;
+			}
+		});
+		$stateProvider.decorator("controllerProvider", function (state, parent) {
+			if (includes(state, "app._games") && state.name !== "app._games") {
+				var dottedNames = state.self.name.split(".");
+				var lastStateName = dottedNames[dottedNames.length - 1];
 
-                var controllerProvider = function($stateParams) {
-                    var capitalizedGame = $stateParams.game[0].toUpperCase() + $stateParams.game.substr(1);
-                    return capitalizedGame + "Controller";
-                };
-                return controllerProvider;
-            } else {
-                return state.self.controllerProvider;
-            }
-        });
+				var controllerProvider = function ($stateParams) {
+					return lastStateName + "_" + $stateParams.game + ".controller";
+				};
+				return controllerProvider;
+			} else {
+				return state.self.controllerProvider;
+			}
+		});
 
-        //w tej fazie nie istnieje jeszcze pole includes, dlatego muszę ręcznie sprawdzić,
-        //czy w nazwie statu jest szukany rodzic
-        function includes(toState, stateName) {
-            return toState.name.search(stateName) >= 0;
-        }
-    }
+		//w tej fazie nie istnieje jeszcze pole includes, dlatego muszę ręcznie sprawdzić,
+		//czy w nazwie statu jest szukany rodzic
+		function includes(toState, stateName) {
+			return toState.name.search(stateName) >= 0;
+		}
+	}
 })();
