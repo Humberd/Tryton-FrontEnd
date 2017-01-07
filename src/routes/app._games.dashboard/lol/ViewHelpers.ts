7e91241df81e41c8d795df362d962521a154e07b
Humberd <sawik11@wp.pl>
2017-01-07 22:06:09 +0100

    removed phases

--- src/routes/app._games.dashboard/lol/ViewHelpers.ts
+++ src/routes/app._games.dashboard/lol/ViewHelpers.ts
@@ -1,7 +1,2 @@
 export class ViewHelpers {
-	public calculateProgress(obj: any): number {
-		let wanted = obj.settings.times.value;
-		let made = wanted - obj.progress.remainingCompletions;
-		return made / wanted * 100;
-	}
 }
