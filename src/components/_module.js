9b12c7fd11fefca3e8601a7abaa73947d48f7b51
Humberd <sawik11@wp.pl>
2017-01-08 18:12:01 +0100

    Revert "fix"
    This reverts commit 56643b42309ac48eebfb3fb12c10f45cb630e2da.

---
+++ src/components/_module.js
@@ -0,0 +1,12 @@
+(function () {
+	"use strict";
+
+	angular.module("TrytonApp.Components", [
+		"TrytonApp.Confirm",
+		"TrytonApp.Loader",
+		"TrytonApp.Login",
+		"TrytonApp.MyTabs",
+		"TrytonApp.Register",
+		"TrytonApp.Toasts"
+	]);
+})();
