058e1d80d77734e2c920a965e6c4c2e691d8f5ee
Humberd <sawik11@wp.pl>
2017-01-15 03:39:57 +0100

    added events to home buttons

--- src/routes/app.home/controller.ts
+++ src/routes/app.home/controller.ts
@@ -8,7 +8,46 @@ class AppHomeController {
 	flag = true;
 	constructor(private $scope,
 				private Loader,
-				private $interval) {
+				private $interval,
+				private Modal,
+				private Session,
+				private $state) {
+	}
 
+	public register(): void {
+		if (!this.Session.isLoggedIn()) {
+			this.goRegister();
+		} else {
+			this.goDashboard();
+		}
+	}
+
+	public dashboard(): void {
+		if (this.Session.isLoggedIn()) {
+			this.goDashboard();
+		} else {
+			this.goLogin();
+		}
+	}
+
+	public faq(): void {
+		console.log("HELLO");
+		this.goFaq();
+	}
+
+	private goDashboard(): void {
+		this.$state.go("app._games.dashboard");
+	}
+
+	private goRegister(): void {
+		this.Modal.register();
+	}
+
+	private goLogin(): void {
+		this.Modal.login();
+	}
+
+	private goFaq(): void {
+		this.$state.go("app.faq");
 	}
 }
